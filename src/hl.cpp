0cf1e07e74a2c8a223634dfc7bc74211283aaa95
creataur <officialcreataur@gmail.com>
2023-03-22 15:04:01 +0530

    Fixed hl.cpp

--- src/hl.cpp
+++ src/hl.cpp
@@ -1,26 +1,19 @@
 /**
   * @file hl.cpp
   * @author Pankaj Sharma (pankajmain@live.com)
-  * @brief The obligatory hello world program to test programming environment
+  * @brief Just to help with layout of VS Code
   * @version 0.1
   * @date 2023-01-21
-  *
-//////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////
+  * //////////////////////////////////////////////////////////////////////////////////////
   * @copyright Copyright (c) 2023
-  *
   */
 
 #include <iostream>
 #include <format>
-#include <utility>
-
 
 int main(int argc, char const* argv[]) {
-  std::cout << std::format("Hello World! Again!\n");
+  std::cout << std::format("Hello World!\n");
   for (size_t i = 0; i < 90; i++)
     std::cout << i % 10;
-  // sumInts(100, 5);
   return 0;
-  ////////////////////////////////////////////////////////////
 }
